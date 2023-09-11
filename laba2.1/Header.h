1ebf177052b2c92cc59c888db817c3fd61833b58
Kopeyka12 <miroshin.vladislav@mail.ru>
2023-09-11 09:25:10 +0900

    испаравлении ошибки

--- laba2.1/Header.h
+++ laba2.1/Header.h
@@ -1,19 +1,87 @@
 #pragma once
+//#include <iostream>
+//Предварительное описание класса «бинарное дерево поиска»
+
 template <class T>
 class BinSTree;
 
+//Определение класса «узел бинарного дерева»
 template <class T>
-class TreeNode {
-private:
-	TreeNode<T>* left;
-	TreeNode<T>* right;
+class TreeNode
+
+{
+    //Указатели на левый и правый порожденные узлы
+
+    TreeNode<T>* left;
+
+    TreeNode<T>* right;
 
 public:
-	T data;
+    //Открытый элемент данных класса, допускающий обновление
 
-	TreeNode(const T& item, TreeNode<T>* lptr = NULL,
-		TreeNode<T>* rptr = NULL);
-	TreeNode<T>* Left(void) const;
-	TreeNode<T>* Right(void) const;
-	friend class BinStree<T>;
+    T data;
+
+    //Конструктор: инициализирует поле данных и указатели
+
+    //на порожденные узлы
+
+    TreeNode(const T& item, TreeNode<T>* lptr = NULL, TreeNode<T>* rptr = NULL);
+
+    //TreeNode(const T& item, TreeNode<T>* lptr, TreeNode<T>* rptr) :
+    //
+    //   data(item), left(lptr), right(rptr) { }
+
+    // Методы доступа к указателям на порожденные узлы
+
+    TreeNode<T>* Left(void) const;
+
+    TreeNode<T>* Right(void) const;
+
+    // Описание класса BinSTree как дружественного
+
+    friend class BinSTree<T>;
+
 };
+
+//Создать объект TreeNode с указательными полями lptr и rptr.
+
+//По умолчанию указатели содержат NULL.
+
+template <class T>
+
+TreeNode<T>::TreeNode(const T& item, TreeNode<T>* lptr, TreeNode<T>* rptr) :
+    
+    data(item), left(lptr), right(rptr) { }
+
+template <class T>
+
+TreeNode<T>* GetTreeNode(T item, TreeNode<T>* lptr = NULL,
+
+    TreeNode<T>* rptr = NULL)
+{
+    TreeNode<T>* p;
+    //Вызвать new для создания нового узла
+    //Передать туда параметры lptr и rptr
+    p = new TreeNode<T>(item, lptr, rptr);
+    //Если памяти недостаточно, завершить
+    //программу сообщением об ошибке
+    if (p == NULL)
+    {
+        std::cerr << "Ошибка при выделении памяти!\n";
+        exit(1);
+    }
+    //Вернуть указатель на выделенную системой память
+    return p;
+}
+
+//Освободить динамическую память, занимаемую данным узлом
+
+template <class T>
+
+void FreeTreeNode(TreeNode<T>* p)
+
+{
+
+    delete p;
+
+}
--- laba2.1/laba2.1.cpp
+++ laba2.1/laba2.1.cpp
@@ -1,150 +1,21 @@
-﻿// laba2.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-#include<iostream>
+﻿// lab 2.1.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "Header.h"
+
 using namespace std;
 
-class BST {
+int main()
+{
+    TreeNode <int>* root, * lchild, * rchild;
+    TreeNode <int>* p;
+    lchild = new TreeNode<int>(20);
+    rchild = new TreeNode<int>(30);
 
-    struct node {
-        int data;
-        node* left;
-        node* right;
-    };
+    root = new TreeNode<int>(10, lchild, rchild);
 
-    node* root;
-
-    node* makeEmpty(node* t) {
-        if (t == NULL)
-            return NULL;
-        {
-            makeEmpty(t->left);
-            makeEmpty(t->right);
-            delete t;
-        }
-        return NULL;
-    }
-
-    node* insert(int x, node* t)
-    {
-        if (t == NULL)
-        {
-            t = new node;
-            t->data = x;
-            t->left = t->right = NULL;
-        }
-        else if (x < t->data)
-            t->left = insert(x, t->left);
-        else if (x > t->data)
-            t->right = insert(x, t->right);
-        return t;
-    }
-
-    node* findMin(node* t)
-    {
-        if (t == NULL)
-            return NULL;
-        else if (t->left == NULL)
-            return t;
-        else
-            return findMin(t->left);
-    }
-
-    node* findMax(node* t) {
-        if (t == NULL)
-            return NULL;
-        else if (t->right == NULL)
-            return t;
-        else
-            return findMax(t->right);
-    }
-
-    node* remove(int x, node* t) {
-        node* temp;
-        if (t == NULL)
-            return NULL;
-        else if (x < t->data)
-            t->left = remove(x, t->left);
-        else if (x > t->data)
-            t->right = remove(x, t->right);
-        else if (t->left && t->right)
-        {
-            temp = findMin(t->right);
-            t->data = temp->data;
-            t->right = remove(t->data, t->right);
-        }
-        else
-        {
-            temp = t;
-            if (t->left == NULL)
-                t = t->right;
-            else if (t->right == NULL)
-                t = t->left;
-            delete temp;
-        }
-
-        return t;
-    }
-
-    void inorder(node* t) {
-        if (t == NULL)
-            return;
-        inorder(t->left);
-        cout << t->data << " ";
-        inorder(t->right);
-    }
-
-    node* find(node* t, int x) {
-        if (t == NULL)
-            return NULL;
-        else if (x < t->data)
-            return find(t->left, x);
-        else if (x > t->data)
-            return find(t->right, x);
-        else
-            return t;
-    }
-
-public:
-    BST() {
-        root = NULL;
-    }
-
-    ~BST() {
-        root = makeEmpty(root);
-    }
-
-    void insert(int x) {
-        root = insert(x, root);
-    }
-
-    void remove(int x) {
-        root = remove(x, root);
-    }
-
-    void display() {
-        inorder(root);
-        cout << endl;
-    }
-
-    void search(int x) {
-        root = find(root, x);
-    }
-};
-
-int main() {
-    BST t;
-    t.insert(20);
-    t.insert(25);
-    t.insert(15);
-    t.insert(10);
-    t.insert(30);
-    t.display();
-    t.remove(20);
-    t.display();
-    t.remove(25);
-    t.display();
-    t.remove(30);
-    t.display();
-    return 0;
+    root->data = 50;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
@@ -156,4 +27,4 @@ int main() {
 //   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
 //   4. В окне "Список ошибок" можно просматривать ошибки.
 //   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
